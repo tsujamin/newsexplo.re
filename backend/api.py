1cb9cd9fc75d847c91533d65faa1c98801db9a45
Andrew Donnellan <andrew@donnellan.id.au>
2016-07-30 13:51:25 +1000

    backend: implement adjacencies api
    Signed-off-by: Andrew Donnellan <andrew@donnellan.id.au>

--- backend/api.py
+++ backend/api.py
@@ -18,8 +18,10 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from flask import jsonify
+from sqlalchemy import or_
 from backend import app
-from backend.orm.models import Content
+from backend.orm.models import Content, Adjacency
+from backend.orm import db_context
 
 @app.route('/api/')
 def hello_world():
@@ -32,7 +34,28 @@ def content_abc(content_id):
 
 @app.route('/api/adjacency/<int:from_id>')
 def get_adjacency(from_id):
-    return jsonify({'apple': 57})
+    content = Content(from_id)
+    adjacencies = db_context.session.query(Adjacency).filter(
+        or_(Adjacency.from_node==from_id, Adjacency.to_node==from_id)).all()
+    print(adjacencies)
+    result = {'id': from_id, 'docType': content.docType, 'title': content.title}
+    result['adjacent_nodes'] = []
+    for adjacency in adjacencies:
+        try:
+            if adjacency.from_node == from_id:
+                new_node = Content(adjacency.to_node)
+            else:
+                new_node = Content(adjacency.from_node)
+
+                result['adjacent_nodes'].append(
+                    {'id': new_node.id,
+                     'docType': new_node.docType,
+                     'title':new_node.title
+                    })
+        except:
+            continue
+
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")
