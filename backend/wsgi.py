3e25b6faaeae4b8727efb83a83bb6af2dfc6151e
govhack2016-server <govhack2016@server>
2016-07-30 10:04:30 +1000

    hello-world flask server configured via nginx/gunicorn

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -14,10 +14,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from api import app
 
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+if __name__ == "__main__":
+    app.run()
