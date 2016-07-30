df622b1ad9d8cf8480fccec7eef793c248c2a144
Benjamin George Roberts <benjamin.roberts@anu.edu.au>
2016-07-30 11:34:36 +1000

    Content model pulls from the ABC API

--- backend/orm/models.py
+++ backend/orm/models.py
@@ -17,6 +17,10 @@
 #
 
 from . import db_context
+import requests
+import json
+
+ABC_API_URL = "https://content-api-govhack.abc-prod.net.au/v1/{}"
 
 _db = db_context
 
@@ -28,6 +32,42 @@ class Content(_db.Model):
     docType = _db.Column( _db.String)
     title = _db.Column(_db.String)
     json = _db.Column(_db.String)
+
+    def __init__(self, _id):
+        """Initialise a new Content object, populate it's fields from the ABC API, save any adjacent nodes and commit"""
+        self.id = _id
+        self._populate_from_abc()
+
+        _db.session.add(self)
+        _db.session.commit()
+
+    def _populate_from_abc(self):
+        """populates the Content instance's fields from the ABC api
+        :returns: true if population was successful
+        """
+        response = requests.get(ABC_API_URL.format(self.id))
+
+        if response.status_code is not 200:
+            raise LookupError("{} does not exist in ABC API".format(self.id))
+
+        self.json_obj = response.json()
+
+        if not ("docType" in self.json_obj and "title" in self.json_obj):
+            raise KeyError("{} does not contain all of docType and title")
+
+        self.docType = self.json_obj["docType"]
+        self.title = self.json_obj["title"]
+        self.json = response.content
+
+    def get_data(self):
+        """
+        Get the cached dumped json object. Note changes to this are not reflected in the database
+        :return: json object
+        """
+        if "json_obj" not in self.__dict__:
+            self.json_obj = json.loads(self.json)
+
+        return self.json_obj
 
 
 class Adjacency(_db.Model):
