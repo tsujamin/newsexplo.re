fbc433b5b1ccf31f0afb202abeca0dcd914e0b79
Andrew Donnellan <andrew@donnellan.id.au>
2016-07-30 11:58:23 +1000

    backend: restructure so it works with subpackages/modules
    Signed-off-by: Andrew Donnellan <andrew@donnellan.id.au>

--- backend/api.py
+++ backend/api.py
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from flask import Flask, jsonify
-app = Flask(__name__)
+from flask import jsonify
+from . import app
 
 @app.route('/api/')
 def hello_world():
