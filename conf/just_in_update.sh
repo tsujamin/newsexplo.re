3f15d626809fbf1afbf63910e7250f8209094573
govhack2016-server <govhack2016@server>
2016-07-31 13:34:13 +1000

    update cron jbos to use environment script

--- conf/just_in_update.sh
+++ conf/just_in_update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 pushd /srv/newsexplo.re
+for line in `cat conf/environment`; do
+	eval "export "$line
+done
 source bin/activate
 python3 -m backend.abc_live.just_in
 popd
--- conf/seed_data_update.sh
+++ conf/seed_data_update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 pushd /srv/newsexplo.re
+for line in `cat conf/environment`; do
+	eval "export "$line
+done
 source bin/activate
 python3 preprocessing/gather_abc_ids.py | python3 preprocessing/query_seed_data.py | sort -n -r -u > preprocessing/ids.txt
 popd
